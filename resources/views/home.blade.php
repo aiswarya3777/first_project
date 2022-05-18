fc302115d80a3f0a8fe70db40d81174e69619907
Aiswarya K <aiswarya3777@gmail.com>
2022-05-18 10:19:29 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('welcome')
+@section('content')
+<h1>Home Page Here we use extends and section</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('index', function () {
+//     return view('welcome');
+// });
+
+// "view" command only to view the page
+Route::view('/','welcome');
+
+//by using the "name" the changes inside the code will not affect the url. 
+Route::view('home','home')->name('home');
+Route::view('index','index')->name('index');
